94671528086b0fa2dbf90f5222f2219a30417ef0
Doug Flynn <dougflynn@yeshealth.com>
2021-12-04 22:21:17 -0500

    Add kubectl apply step

---
+++ pages/requests/index.tsx
@@ -0,0 +1,8 @@
+import type { NextPage } from "next";
+import { SongRequests } from "../../lib/pages/SongRequests";
+
+const Home: NextPage = () => {
+    return <SongRequests />;
+};
+
+export default Home;
